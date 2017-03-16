eafec86882208dcf21519f5055bfafbaa94f3c7b
dmariage <d.mariage@groupeonepoint.com>
2017-03-16 14:30:00 +0100

    fix labels and password
    now handle multiple labels and passwords containing special chars

--- cmd.sh
+++ cmd.sh
@@ -8,13 +8,13 @@ if [ ! -z "$JENKINS_USERNAME" ]; then
   PARAMS="$PARAMS -username $JENKINS_USERNAME"
 fi
 if [ ! -z "$JENKINS_PASSWORD" ]; then
-  PARAMS="$PARAMS -password $JENKINS_PASSWORD"
+  PARAMS="$PARAMS -password '$JENKINS_PASSWORD'"
 fi
 if [ ! -z "$SLAVE_EXECUTORS" ]; then
   PARAMS="$PARAMS -executors $SLAVE_EXECUTORS"
 fi
 if [ ! -z "$SLAVE_LABELS" ]; then
-  PARAMS="$PARAMS -labels $SLAVE_LABELS"
+  PARAMS="$PARAMS -labels '$SLAVE_LABELS'"
 fi
 if [ ! -z "$SLAVE_NAME" ]; then
   PARAMS="$PARAMS -name $SLAVE_NAME"
